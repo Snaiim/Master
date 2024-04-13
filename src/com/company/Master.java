ac7d149b515bdfe432a90c5149ea86f4f1215590
Admin <satanovaa06@gmail.com>
2024-04-13 12:45:29 +0600

    Defence

---
+++ src/com/company/Master.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Master {
+    public static void master(String[] args) {
+        String str = " 1000";
+        int count = 0;
+        char ch = 1;
+
+        for (int num : ch) {
+            if()
+
+        }
+
+        System.out.println("0ccurrences" + ":" + count);
+    }
+}
